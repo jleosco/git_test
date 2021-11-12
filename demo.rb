cc21a293f7844ed9f0bb58da3bccbd03a2da9ed1
John Leosco <jrleosco@gmail.com>
2021-11-12 12:50:38 -0700

    added comments

--- demo.rb
+++ demo.rb
@@ -1,3 +1,28 @@
 puts "hello world"
 
 puts "1 + 1 = 2"
+
+
+
+# setup do this for each project
+# git init to create a repo (rails does this for you)
+# create a repo on github
+# add the remote link (remeber to use the ssh link on github)
+
+# basic flow
+# make change in your code
+# add(stage) those changes git add .   or git add filename
+# commit those changes git commit -m'description of what you did'
+
+# push step: when needed
+# pull from github (if needed)
+# fix any conflicts if they exist
+# push up to github
+
+
+# if working with a partner
+# one of you will create the repo, the other will clone done repo(once)
+
+# When a team member pushes to master, they should tell everyone to
+# pull from master (ie git pull origin master)
+
